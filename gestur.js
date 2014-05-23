dc8ba2790d0cb80fc16ad12eb09439949a87696f
catoio <rcatoio@wyplay.com>
2014-05-23 07:50:38 +0200

    debug ipad gesture

--- gestur.js
+++ gestur.js
@@ -1,64 +1,65 @@
-var Gesture = function(elem)
-{
+var Gesture = function (elem) {
+    'use strict';
     this.element = elem;
-    this.fingersNumber= 0;
-    this.fingers=[];
-    this.init= function(){
+    this.fingers = {};
+    this.init = function () {
         var self = this;
-        elem.addEventListener('touchstart', self.onTouchStart= function(e){
-            self.fingersNumber++;
-            self.gestureType=1;
+        elem.addEventListener('touchstart', self.onTouchStart = function (e) {
+            self.gestureType = 1;
             var finger = {
                 startX: e.changedTouches[0].pageX,
                 startY: e.changedTouches[0].pageY
             };
             self.fingers[e.changedTouches[0].identifier] = finger;
-
-        },false);
-        elem.addEventListener('touchmove', self.onTouchMove = function(e){
-            for(var i =0; i < e.touches.length; i++){
+        }, false);
+        elem.addEventListener('touchmove', self.onTouchMove = function (e) {
+            for (var i = 0; i < e.touches.length; i++) {
                 var touche = e.touches[i];
-                if(!self.fingers[touche.identifier]){
+                if (!self.fingers[touche.identifier]) {
                     self.fingers[touche.identifier] = {};
-                    self.fingers[touche.identifier].startX= touche.pageX;
-                    self.fingers[touche.identifier].startY= touche.pageY;
+                    self.fingers[touche.identifier].startX = touche.pageX;
+                    self.fingers[touche.identifier].startY = touche.pageY;
                 }
-                self.fingers[touche.identifier].stopX =touche.pageX;
-                self.fingers[touche.identifier].stopY =touche.pageY;
+                self.fingers[touche.identifier].stopX = touche.pageX;
+                self.fingers[touche.identifier].stopY = touche.pageY;
             }
-        },false);
-        elem.addEventListener('touchend', self.onTouchStop = function(e){
-            self.fingersNumber--;
-            if(self.fingersNumber === 0){
+        }, false);
+        elem.addEventListener('touchend', self.onTouchStop = function (e) {
+            if (e.touches.length === 0) {
                 //gesture is ending
                 var event = self.getRealEvent(e);
                 self.createEvent(event);
-                self.fingers = [];
+                self.fingers = {};
             }
-        },false);
+        }, false);
     };
     this.init();
-    this.getRealEvent = function(originalEvent){
+    this.getRealEvent = function (originalEvent) {
         var self = this;
         var event = {};
+        var distX;
+        var distY;
+        var distX2;
+        var distY2;
         event.originalSrcElement = originalEvent.srcElement;
-        event.fingers = self.fingers.length;
-        switch( self.fingers.length ){
+        var fingerKey = Object.keys(self.fingers);
+        event.fingers = fingerKey.length;
+        switch (event.fingers) {
             case 1:
-                var distX = self.fingers[0].startX-self.fingers[0].stopX;
-                var distY = self.fingers[0].startY-self.fingers[0].stopY;
-                if(Math.abs(distX)+Math.abs(distY)> 200){
+                distX = self.fingers[fingerKey[0]].startX - self.fingers[fingerKey[0]].stopX;
+                distY = self.fingers[fingerKey[0]].startY - self.fingers[fingerKey[0]].stopY;
+                if (Math.abs(distX) + Math.abs(distY) > 200) {
                     event.eventName = 'swipe';
-                    if( Math.abs(distX) > Math.abs(distY) ){
-                        if(distX>0){
+                    if (Math.abs(distX) > Math.abs(distY)) {
+                        if (distX > 0) {
                             event.direction = 'left';
-                        }else {
+                        } else {
                             event.direction = 'right';
                         }
                     } else {
-                        if(distY>0){
+                        if (distY > 0) {
                             event.direction = 'top';
-                        }else {
+                        } else {
                             event.direction = 'bottom';
                         }
                     }
@@ -68,34 +69,34 @@ var Gesture = function(elem)
                 }
                 break;
             case 2:
-                var distX = self.fingers[0].startX-self.fingers[0].stopX;
-                var distY = self.fingers[0].startY-self.fingers[0].stopY;
-                var distX2 = self.fingers[1].startX-self.fingers[1].stopX;
-                var distY2 = self.fingers[1].startY-self.fingers[1].stopY;
-                if(Math.abs(distX)+Math.abs(distY) + Math.abs(distX2)+Math.abs(distY2)> 200){
-                    if(distX*distX2> 0 || distY*distY2> 0){
+                distX = self.fingers[fingerKey[0]].startX - self.fingers[fingerKey[0]].stopX;
+                distY = self.fingers[fingerKey[0]].startY - self.fingers[fingerKey[0]].stopY;
+                distX2 = self.fingers[fingerKey[1]].startX - self.fingers[fingerKey[1]].stopX;
+                distY2 = self.fingers[fingerKey[1]].startY - self.fingers[fingerKey[1]].stopY;
+                if (Math.abs(distX) + Math.abs(distY) + Math.abs(distX2) + Math.abs(distY2) > 200) {
+                    if (distX * distX2 > 0 || distY * distY2 > 0) {
                         //same direction fingers
                         event.eventName = 'swipe';
-                        if( Math.abs(distX) > Math.abs(distY) ){
-                            if(distX>0){
+                        if (Math.abs(distX) > Math.abs(distY)) {
+                            if (distX > 0) {
                                 event.direction = 'left';
-                            }else {
+                            } else {
                                 event.direction = 'right';
                             }
                         } else {
-                            if(distY>0){
+                            if (distY > 0) {
                                 event.direction = 'top';
-                            }else {
+                            } else {
                                 event.direction = 'bottom';
                             }
                         }
                     } else {
                         event.eventName = 'pinch';
-                        var distanceBefore = self.distance(self.fingers[0].startX, self.fingers[0].startY,
-                            self.fingers[1].startX, self.fingers[1].startY);
-                        var distanceAfter = self.distance(self.fingers[0].stopX, self.fingers[0].stopY,
-                            self.fingers[1].stopX, self.fingers[1].stopY)
-                        if(distanceBefore > distanceAfter){
+                        var distanceBefore = self.distance(self.fingers[fingerKey[0]].startX, self.fingers[fingerKey[0]].startY,
+                            self.fingers[fingerKey[1]].startX, self.fingers[fingerKey[1]].startY);
+                        var distanceAfter = self.distance(self.fingers[fingerKey[0]].stopX, self.fingers[fingerKey[0]].stopY,
+                            self.fingers[fingerKey[1]].stopX, self.fingers[fingerKey[1]].stopY);
+                        if (distanceBefore > distanceAfter) {
                             event.direction = 'out';
                         } else {
                             event.direction = 'in';
@@ -109,14 +110,14 @@ var Gesture = function(elem)
         }
         return event;
     };
-    this.distance = function(x1, y1, x2, y2){
-        var sqr = function(a){
-            return a*a;
-        }
+    this.distance = function (x1, y1, x2, y2) {
+        var sqr = function (a) {
+            return a * a;
+        };
         return Math.sqrt(sqr(y2 - y1) + sqr(x2 - x1));
     };
-    this.createEvent = function(realEvent){
-        var event; // The custom event that will be created
+    this.createEvent = function (realEvent) {
+        var event;
         var self = this;
         if (document.createEvent) {
             event = document.createEvent("HTMLEvents");
@@ -125,10 +126,10 @@ var Gesture = function(elem)
             event = document.createEventObject();
             event.eventType = realEvent.eventName;
         }
-        eventObjectKeys = Object.keys(realEvent);
+        var eventObjectKeys = Object.keys(realEvent);
 
-        for(var i = 0; i < eventObjectKeys.length; i++){
-            event[eventObjectKeys[i]]= realEvent[eventObjectKeys[i]];
+        for (var i = 0; i < eventObjectKeys.length; i++) {
+            event[eventObjectKeys[i]] = realEvent[eventObjectKeys[i]];
         }
         if (document.createEvent) {
             self.element.dispatchEvent(event);
@@ -136,4 +137,4 @@ var Gesture = function(elem)
             self.element.fireEvent("on" + event.eventType, event);
         }
     };
-}
+};
